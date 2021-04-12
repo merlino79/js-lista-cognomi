942ae193414437e8d34710b7322de72232c31093
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-12 15:53:13 +0200

    prova ciclo for lista stamata

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 //creazione lista 
-var emails = [
-    "luca@gmail.com",
-    "marco@libero.it",
-    "serena@libero.it ",
-    "tey@libero.it",
-    "gianluca@yahoo.it",
-    "alessandro@hotmail.com",
-    "simone@gmail.com",
-    "martino@gmail.com",
-    "silvia@gmail.com"
+var cognomi = [
+    "ciancio",
+    "leonovo",
+    "acer ",
+    "samsung",
+    "apple",
+    "deblasi",
+    "perlina",
+    "rossi",
+    "ciancio paratore"
 ];
+
+console.log(cognomi);
+console.log(cognomi.length - 1);
+
+
+for (i = 0; i < cognomi.length; i++) {
+    var surname = cognomi[i]
+    console.log(surname)
+
+}
