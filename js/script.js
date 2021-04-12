9874001304260ec6b0f505d2371a355b42a36ab8
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-12 15:44:05 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//creazione lista 
+var emails = [
+    "luca@gmail.com",
+    "marco@libero.it",
+    "serena@libero.it ",
+    "tey@libero.it",
+    "gianluca@yahoo.it",
+    "alessandro@hotmail.com",
+    "simone@gmail.com",
+    "martino@gmail.com",
+    "silvia@gmail.com"
+];
